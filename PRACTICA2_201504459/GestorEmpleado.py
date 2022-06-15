1dc5280322f9cc389f7e4a51930656bd43a26883
Dayanira López <sdayaniralgomez@gmail.com>
2022-06-14 22:10:48 -0600

    Practica2 empleado

--- PRACTICA2_201504459/GestorEmpleado.py
+++ PRACTICA2_201504459/GestorEmpleado.py
@@ -6,12 +6,65 @@ class GestrorEmpleado:
         raiz = archivo_xml.getroot()
         i=0
         cadena = ""
-        for padre in raiz:
-            while i <= len(padre)-1:
-                cadena = cadena + "\nID: " + str(padre[i].get('id')) + "\nNombre: "+ str(padre[i][0].text)+"\nPuesto: " + str(padre[i][1].text)+"\nSalario: " + str(padre[i][2].text)
-                i=i+1
-            i = 0
-        return cadena
+        if raiz.tag == "empresa":
+            for padre in raiz:
+                while i <= len(padre)-1:
+                    cadena = cadena + "\nID: " + str(padre[i].get('id')) + "\nNombre: "+ str(padre[i][0].text)+"\nPuesto: " + str(padre[i][1].text)+"\nSalario: " + str(padre[i][2].text)
+                    i=i+1
+                i = 0
+            return cadena
+        elif  raiz.tag == "catalog":
+            return "No hay informacion"
 
+    def buscar(self,id,ruta):
+        archivo_xml = ET.parse(ruta)
+        raiz = archivo_xml.getroot()
+        i = 0
+        if raiz.tag == "empresa":
+            for padre in raiz:
+                while i <= len(padre) - 1:
+                    if id== padre[i].get('id'):
+                        nombre = str(padre[i][0].text)
+                        puesto = str(padre[i][1].text)
+                        salario =str(padre[i][2].text)
+                    i = i + 1
+                i = 0
+            return nombre,puesto,salario
+        elif raiz.tag == "catalog":
+            return "No hay informacion"
+
+
+    def modificar(self,id,ruta,nombre,puesto,salario):
+        archivo_xml = ET.parse(ruta)
+        raiz = archivo_xml.getroot()
+        i = 0
+        if raiz.tag == "empresa":
+            for padre in raiz:
+                while i <= len(padre) - 1:
+                    if id== padre[i].get('id'):
+                       padre[i][0].text=nombre
+                       padre[i][1].text = puesto
+                       padre[i][2].text = salario
+                    i = i + 1
+                i = 0
+
+
+    def Eliminar(self, id, ruta):
+        archivo_xml = ET.parse(ruta)
+        raiz = archivo_xml.getroot()
+        i = 0
+        if raiz.tag == "empresa":
+            for departamento in raiz:
+                for empleado in departamento.findall('empleado'):
+                    idE = int(empleado.get('id'))
+                    print(idE)
+                    if idE== int(id):
+                        departamento.remove(empleado)
+
+
+    def GenerarNuevo(self,ruta):
+        archivo_xml = ET.parse(ruta)
+        nombre_archivo = ruta
+        archivo_xml.write(nombre_archivo, xml_declaration=True, encoding="utf-8")
 
 
--- PRACTICA2_201504459/main.py
+++ PRACTICA2_201504459/main.py
@@ -44,6 +44,9 @@ class Ventana():
 
     def Ventana_Empleado(self):
         self.idEmpelado = StringVar()
+        self.nombre = StringVar()
+        self.salario = StringVar()
+        self.puesto = StringVar()
         self.windowE= Toplevel()
         self.windowE.title("EMPLEADOS")
         self.windowE.geometry('600x300')
@@ -53,7 +56,7 @@ class Ventana():
         Titulo.place(x=150, y=5)
 
         self.Texto = Text(self.windowE)
-        self.Texto.config(width=30, height=13, font=("Consolas", 8), selectbackground="blue")
+        self.Texto.config(width=35, height=13, font=("Consolas", 8), selectbackground="blue")
         self.Texto.place(x=350, y=60)
 
         IdEmpleado = Label(self.windowE, text="ID Empleado", bg='#9DB7E1',font=(fuente, 11))
@@ -62,34 +65,64 @@ class Ventana():
         txtID = tkinter.Entry(self.windowE, textvariable=self.idEmpelado)
         txtID.place(x=150, y=50)
 
-        Buscar = Button(self.windowE, text="Buscar Empleado", font=(fuente, 10), bg='#AFC9D3')
+        Buscar = Button(self.windowE, text="Buscar Empleado", font=(fuente, 10), bg='#AFC9D3', command=self.Buscar)
         Buscar.place(x=100, y=100)
 
         Nombre = Label(self.windowE, text="Nombre", bg='#9DB7E1', font=(fuente, 11))
         Nombre.place(x=35, y=150)
 
-        txtNombre = tkinter.Entry(self.windowE, textvariable=self.idEmpelado)
+        txtNombre = tkinter.Entry(self.windowE, textvariable=self.nombre)
         txtNombre.place(x=150, y=150)
 
+        Puesto = Label(self.windowE, text="Puesto", bg='#9DB7E1', font=(fuente, 11))
+        Puesto.place(x=35, y=180)
+
+        txtPuesto = tkinter.Entry(self.windowE, textvariable=self.puesto)
+        txtPuesto.place(x=150, y=180)
+
         Salario = Label(self.windowE, text="Salario", bg='#9DB7E1', font=(fuente, 11))
-        Salario.place(x=35, y=200)
+        Salario.place(x=35, y=210)
 
-        txtSalario = tkinter.Entry(self.windowE, textvariable=self.idEmpelado)
-        txtSalario.place(x=150, y=200)
+        txtSalario = tkinter.Entry(self.windowE, textvariable=self.salario)
+        txtSalario.place(x=150, y=210)
 
-        Modificar = Button(self.windowE, text="Modificar Empleado", font=(fuente, 10), bg='#AFC9D3')
+        Modificar = Button(self.windowE, text="Modificar Empleado", font=(fuente, 10), bg='#AFC9D3', command=self.ModificarE)
         Modificar.place(x=10, y=250)
 
-        Eliminar = Button(self.windowE, text="Eliminar Empleado", font=(fuente, 10), bg='#AFC9D3')
+        Eliminar = Button(self.windowE, text="Eliminar Empleado", font=(fuente, 10), bg='#AFC9D3',command=self.Eliminar)
         Eliminar.place(x=180, y=250)
 
         Vtodo = Button(self.windowE, text="Ver Todo", font=(fuente, 10), bg='#AFC9D3', command=self.Vertodo)
-        Vtodo.place(x=400, y=250)
+        Vtodo.place(x=350, y=250)
+        GNuevo = Button(self.windowE, text="Generar Archivo", font=(fuente, 10), bg='#AFC9D3', command=self.GenerarNuevo)
+        GNuevo.place(x=450, y=250)
 
 
     def Vertodo(self):
-        texto =enviar.imprimir(self.ruta.get())
+        self.Texto.delete('0.0',"end")
+        texto = enviar.imprimir(self.ruta.get())
         self.Texto.insert('0.0', texto)
+
+    def Buscar(self):
+        datos = enviar.buscar(self.idEmpelado.get(),self.ruta.get())
+        self.nombre.set(datos[0])
+        self.puesto.set(datos[1])
+        self.salario.set(datos[2])
+
+
+    def ModificarE(self):
+        enviar.modificar(self.idEmpelado.get(),self.ruta.get(),self.nombre.get(),self.puesto.get(),self.salario.get())
+
+
+    def Eliminar(self):
+        enviar.Eliminar(self.idEmpelado.get(),self.ruta.get())
+
+    def GenerarNuevo(self):
+        enviar.GenerarNuevo(self.ruta.get())
+
+
+
+
 
 def main():
     Ventana()
